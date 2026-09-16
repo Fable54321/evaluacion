851432e2e5a04fa416264fe4acf5f5a7a68572b0
fable54321 <timothe5@hotmail.com>
2026-09-16 09:05:16 -0400

    first commit

--- src/App/100--Home/Home.tsx
+++ src/App/100--Home/Home.tsx
@@ -3,11 +3,11 @@ import vegibecLogo from "../../assets/vegibec.png"
 
 const Home = () => {
   return (
-    <article className= "flex flex-col items-center gap-8 font-primary">
+    <article className= "flex flex-col items-center md:gap-8 gap-5 font-primary">
       <div className="w-[min(98%,700px)] mt-20">
         <img src={vegibecLogo} alt="Vegibec" />
       </div>
-      <div className="flex flex-col gap-4 text-center w-[min(98%,600px)]">
+      <div className="flex flex-col md:gap-4 gap-1 text-center w-[min(98%,600px)]">
         <Link to="evaluaciones-mensuales" className="bg-secondary text-white p-2 md:text-[1.7rem] tracking-tight md:scale-y-120 rounded-lg hover:bg-primary active:scale-x-98 active:scale-y-105">Evaluaciones mensuales</Link>
         <Link to="variacion-de-desempeno" className="bg-secondary text-white p-2 md:text-[1.7rem] tracking-tight md:scale-y-120 rounded-lg hover:bg-primary active:scale-x-98 active:scale-y-105">Variación de desempeño</Link>
       </div>
