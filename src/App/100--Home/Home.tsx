4adfe06f326f2eb720e6737dde3ce30dfb36964f
fable54321 <timothe5@hotmail.com>
2026-09-16 09:03:32 -0400

    first commit

--- src/App/100--Home/Home.tsx
+++ src/App/100--Home/Home.tsx
@@ -4,12 +4,12 @@ import vegibecLogo from "../../assets/vegibec.png"
 const Home = () => {
   return (
     <article className= "flex flex-col items-center gap-8 font-primary">
-      <div className="w-130 mt-20">
+      <div className="w-[min(98%,700px)] mt-20">
         <img src={vegibecLogo} alt="Vegibec" />
       </div>
-      <div className="flex flex-col gap-4">
-        <Link to="evaluaciones-mensuales" className="bg-secondary text-white p-2 text-[1.7rem] tracking-tight scale-y-120 rounded-lg hover:bg-primary active:scale-x-98 active:scale-y-105">Evaluaciones mensuales</Link>
-        <Link to="variacion-de-desempeno" className="bg-secondary text-white p-2 text-[1.7rem] tracking-tight scale-y-120 rounded-lg hover:bg-primary active:scale-x-98 active:scale-y-105">Variación de desempeño</Link>
+      <div className="flex flex-col gap-4 text-center w-[min(98%,600px)]">
+        <Link to="evaluaciones-mensuales" className="bg-secondary text-white p-2 md:text-[1.7rem] tracking-tight md:scale-y-120 rounded-lg hover:bg-primary active:scale-x-98 active:scale-y-105">Evaluaciones mensuales</Link>
+        <Link to="variacion-de-desempeno" className="bg-secondary text-white p-2 md:text-[1.7rem] tracking-tight md:scale-y-120 rounded-lg hover:bg-primary active:scale-x-98 active:scale-y-105">Variación de desempeño</Link>
       </div>
     </article>
   )
